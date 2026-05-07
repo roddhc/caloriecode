4d219a4939ea569d6005271d558e565e39e23a78
Rod Lamont <roddhc@gmail.comm>
2026-05-07 23:33:42 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+//lib/main.dart
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CalorieCodeApp());
+}
+
+class CalorieCodeApp extends StatelessWidget {
+  const CalorieCodeApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CalorieCode',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text('CalorieCode'),
+        ),
+      ),
+    );
+  }
+}
